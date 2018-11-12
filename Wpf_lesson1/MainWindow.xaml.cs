0ef0026fe1514cc6e7ca757f2b9ee6723fb423c8
Valencia Linda <LindaDValencia@outlook.com>
2018-11-12 20:35:24 +0800

    update_button

--- Wpf_lesson1/MainWindow.xaml.cs
+++ Wpf_lesson1/MainWindow.xaml.cs
@@ -24,5 +24,44 @@ namespace Wpf_lesson1
         {
             InitializeComponent();
         }
+
+        private void rotatedButton_Click(object sender, RoutedEventArgs e)
+        {
+             
+        }
+
+        private void rotateButton_KeyDown(object sender, KeyEventArgs e)
+        {
+            MessageBox.Show("rotatedButton handler, bubbling up");
+        }
+
+        private void Grid_KeyDown(object sender, KeyEventArgs e)
+        {
+            MessageBox.Show("Grid handler, bubbling up");
+
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            MessageBox.Show("Window handler, bubbling up");
+        }
+
+        private void rotatedButton_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            MessageBox.Show("rotatedButton handler, tunneing down");
+        }
+
+        private void Grid_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            MessageBox.Show("Grid handler, tunneing down");
+           // e.Handled = true;  //停止调用路由事件
+
+        }
+
+        private void Windown_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            MessageBox.Show("Window handler, tunneing down");
+        }
     }
 }
+//enter、tab、esc、blank、orientation key is not means input 
