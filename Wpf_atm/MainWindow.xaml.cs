0a690bb43435baa246ead003a22aa0e7fec5b9f1
Valencia Linda <LindaDValencia@outlook.com>
2018-11-18 17:15:19 +0800

    wpf_atm

---
+++ Wpf_atm/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+
+namespace Wpf_atm
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string[] accountTypeArr = new string[] { "借记卡", "贷记卡" };
+            accountTypeBox.ItemsSource = accountTypeArr; //设置下拉菜单的可选项
+            accountTypeBox.SelectedIndex = 0;  //默认选中第0项
+
+
+
+        }
+
+        private void AcceptButton_Click(object sender, RoutedEventArgs e)
+        {
+            //。。do somthing
+            MessageBox.Show("打印凭条");
+        }
+    }
+}
---
+++ Wpf_checkbox_radiobutton/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpf_checkbox_radiobutton
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+            /*
+             * 在wpf中绑定comboBox的值，Dictionary【可运行】【后台绑定】放在public MainWindow下
+             前台：
+             <ComboBox Name="combobox" Width="120" Height="30"/>
+            后台：
+               Dictionary<int, string> mydic = new Dictionary<int, string>()
+            {
+                {1,"a"},
+                {2,"b"},
+                {3,"c"}
+            };
+            combobox.ItemsSource = mydic;
+            combobox.SelectedValuePath = "Key";
+            combobox.DisplayMemberPath = "Value";             
+             */
+
+
+
+        }
+
+
+
+    }
+}
