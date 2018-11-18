cf99d3ae450fab126c98c2dacc9ea617cda1b07f
Valencia Linda <LindaDValencia@outlook.com>
2018-11-18 23:48:35 +0800

    wpf-atm

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string num = "0.4545454545";
+            string num1 = "0.121212124564564564561";
+            Console.WriteLine(double.Parse(num)+ double.Parse(num1));
+        }
+    }
+}
--- Wpf_atm/MainWindow.xaml.cs
+++ Wpf_atm/MainWindow.xaml.cs
@@ -28,15 +28,38 @@ namespace Wpf_atm
             string[] accountTypeArr = new string[] { "借记卡", "贷记卡" };
             accountTypeBox.ItemsSource = accountTypeArr; //设置下拉菜单的可选项
             accountTypeBox.SelectedIndex = 0;  //默认选中第0项
-
-
-
         }
 
         private void AcceptButton_Click(object sender, RoutedEventArgs e)
         {
             //。。do somthing
             MessageBox.Show("打印凭条");
+        }
+
+        private void MoneyLogic() {
+            if (InputAmount.Text == "") {
+                MessageBox.Show("请输入操作金额！！");
+                return;
+            }
+            //存款
+            if (rdb_ck.IsChecked == true) {
+                cnyTotalBalance.Text = (Math.Round(Decimal.Parse(cnyTotalBalance.Text.ToString()) + Decimal.Parse(InputAmount.Text.ToString()),2)).ToString();
+            }
+            //取款
+            if (rdb_qk.IsChecked == true) {
+                cnyTotalBalance.Text = (Math.Round(Decimal.Parse(cnyTotalBalance.Text.ToString()) - Decimal.Parse(InputAmount.Text.ToString()), 2)).ToString();
+            }
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            MoneyLogic();
+        }
+
+
+        private void Rdb_qk_Checked(object sender, RoutedEventArgs e)
+        {
+            MoneyLogic();
         }
     }
 }
